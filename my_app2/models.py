d589bedbbcc79814ff9c37e85e5e45a637cc91f5
vivekguttis <vivekgutti321@gmail.com>
2023-06-12 15:58:26 +0530

    next set of fils

--- my_app2/models.py
+++ my_app2/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+    gender=[
+        ("m","male"),
+        ("f","female"),
+        ("o","others"),
+    ]
+    fname=models.CharField(max_length=50)
+    lname=models.CharField(max_length=50)
+    age=models.IntegerField(default=0,null=True)
+    email=models.EmailField()
+    gender=models.CharField(max_length=1,choices=gender)
